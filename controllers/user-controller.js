8fbe3570040be0cfca73246a36ba02d74347fe80
Erikson <chris.s.erickson@gmail.com>
2022-10-17 11:54:30 -0400

    started user contollers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,10 +4,25 @@ const { User } = require("../models");
 // User Controller
 const UserController = {
     // get all users
+    getAllUsers(req, res) {
+
+    },
     // get single user w _id
+    getUserById(req, res) {
+        
+    },
     // post new user
+    createUser(req, res) {
+        
+    },
     // put update user w _id
+    updateUser(req, res) {
+        
+    },
     // Delete user w _id -- delete associated thoughts and reactions
+    deleteUser(req, res){
+        
+    }
 }
 
 
